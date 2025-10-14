27d1c2cb71a31c62f1c30f8fdc7bd927c226a5d6
Julien Logette <julien.logette@gmail.com>
2025-10-14 21:32:37 +0200

    modif program.cs pour test estpair

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// using System;
-Console.WriteLine("Les pipelines, c’est trop bien !");
+﻿using System;
+bool EstPair(int n) => n % 2 == 0;
+if (EstPair(4))
+{
+    Console.WriteLine("Test 1 OK : 4 est pair");
+}
+else
+{
+    Console.WriteLine("Test 1 ÉCHEC");
+    Environment.Exit(1);
+}
+if (!EstPair(5))
+{
+    Console.WriteLine("Test 2 OK : 5 est impair");
+}
+else
+{
+    Console.WriteLine("Test 2 ÉCHEC");
+    Environment.Exit(1);
+}
+Console.WriteLine("Tous les tests sont passés !");
