e2555a3430400cecbefa29b198e6f51f164cb4ad
Julien Logette <julien.logette@gmail.com>
2025-10-17 15:00:41 +0200

    Test PR avec pipeline de vérification

--- Program.cs
+++ Program.cs
@@ -1,10 +1,2 @@
 ﻿using System;
-var accessToken = Environment.GetEnvironmentVariable("ACCESS_TOKEN");
-if (accessToken.StartsWith("mo"))
-{
-    Console.WriteLine("Accès autorisé !");
-}
-else
-{
-    Console.WriteLine("Accès refusé");
-}
+Console.Writeline("Hello, Wolrd!");
